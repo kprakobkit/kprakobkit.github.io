746a368033b671da08b2bb27cf879009165d92a3
kprakobkit <kprakobkit@gmail.com>
2016-10-03 19:38:04 -0700

    Edits bio

--- components/Bio.js
+++ components/Bio.js
@@ -4,12 +4,17 @@ import { rhythm } from 'utils/typography'
 class Bio extends React.Component {
   render () {
     return (
-      <p
-        style={{
-          marginBottom: rhythm(1.75),
-        }}
-      >I'm a software developer for <a href="https://www.thoughtworks.com" target="_blank">ThoughtWorks</a>. I wrote these posts to share my learning and to <a href="https://www.urbandictionary.com/define.php?term=pay%20it%20forward" target="_blank">pay it forward</a>.
+      <div>
+      <p>
+        Hi there! My name's Peter and I'm a software developer working for <a href="https://www.thoughtworks.com" target="_blank">ThoughtWorks</a> in San Francisco.
       </p>
+      <p
+      style={{
+        marginBottom: rhythm(1.75),
+      }}
+      >I wrote these posts to share my learning and <a href="https://www.urbandictionary.com/define.php?term=pay%20it%20forward" target="_blank">pay it forward</a> and would love to hear from you so please don't hesitate to <a href="mailto:kprakobkit+blog@gmail.com">email me</a>.
+      </p>
+      </div>
     )
   }
 }
