fa13aa321d5e8fa4c09c4496795aaf62e8d4c153
kprakobkit <kprakobkit@gmail.com>
2016-10-05 11:35:15 -0700

    Removes react GA, instead resort back to inserting script tag

--- html.js
+++ html.js
@@ -2,7 +2,6 @@ import React from 'react'
 import DocumentTitle from 'react-document-title'
 import { prefixLink } from 'gatsby-helpers'
 import { TypographyStyle } from 'utils/typography'
-import ReactGA from 'react-ga';
 
 const BUILD_TIME = new Date().getTime()
 
@@ -18,7 +17,6 @@ module.exports = React.createClass({
     let css
     if (process.env.NODE_ENV === 'production') {
       css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
-      ReactGA.initialize('UA-37050244-3');
     }
 
     let script
@@ -42,6 +40,14 @@ module.exports = React.createClass({
         <body className="landing-page">
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: body }} />
           {script}
+           <script dangerouslySetInnerHTML={{__html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-37050244-3', 'auto');
+            ga('send', 'pageview');
+          `}} />
         </body>
       </html>
     )
